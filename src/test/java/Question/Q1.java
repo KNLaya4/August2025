9a68ccc2ef8ffe9504759e313e0f7887b27b86d0
KNLaya4 <kn.laya.4@gmail.com>
2025-08-26 15:12:41 +0530

    done

---
+++ src/test/java/Question/Q1.java
@@ -0,0 +1,30 @@
+package Question;
+
+import org.testng.annotations.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Q1{
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Page Title: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
